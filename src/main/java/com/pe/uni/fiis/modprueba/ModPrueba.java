14e530cfa371d6633145e2d1cc3de422f5063e2c
Oscar <oscar.torres.m@uni.pe>
2021-12-29 18:14:37 -0500

    Cambiando descripcion y imagen del mod

--- src/main/java/com/pe/uni/fiis/modprueba/ModPrueba.java
+++ src/main/java/com/pe/uni/fiis/modprueba/ModPrueba.java
@@ -1,4 +1,5 @@
-package com.example.examplemod;
+package com.pe.uni.fiis.modprueba;
+
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -18,13 +19,14 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(ModPrueba.Mod_ID)
+public class ModPrueba
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    public static final String Mod_ID = "modprueba";
 
-    public ExampleMod() {
+    public ModPrueba() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -32,8 +34,10 @@ public class ExampleMod
         // Register the processIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
 
+        // event    
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
     }
 
     private void setup(final FMLCommonSetupEvent event)
