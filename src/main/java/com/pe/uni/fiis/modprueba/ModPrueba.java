b3f7ec3e02366ff327b0b611f951975fd72bd251
Oscar <oscar.torres.m@uni.pe>
2021-12-29 23:44:47 -0500

    agregado item personalizado

--- src/main/java/com/pe/uni/fiis/modprueba/ModPrueba.java
+++ src/main/java/com/pe/uni/fiis/modprueba/ModPrueba.java
@@ -1,48 +1,32 @@
 package com.pe.uni.fiis.modprueba;
 
+import com.pe.uni.fiis.modprueba.item.ModItem;
 
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
-import com.pe.uni.fiis.modprueba.item.ModItem;
-
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ModPrueba.Mod_ID)
 
-public class ModPrueba
-{
+public class ModPrueba {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String Mod_ID = "modprueba";
 
     public ModPrueba() {
-        
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
         ModItem.register(eventBus);
-        
         eventBus.addListener(this::setup);
-
         MinecraftForge.EVENT_BUS.register(this);
-
     }
 
-    private void setup(final FMLCommonSetupEvent event){
+    private void setup(final FMLCommonSetupEvent event) {
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
--- src/main/java/com/pe/uni/fiis/modprueba/blocks/ModBlock.java
+++ src/main/java/com/pe/uni/fiis/modprueba/blocks/ModBlock.java
@@ -1,11 +1,28 @@
-/*package com.pe.uni.fiis.modprueba.blocks;
+// package com.pe.uni.fiis.modprueba.blocks;
 
-import org.antlr.v4.parse.ANTLRParser.blockEntry_return;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
+// import com.pe.uni.fiis.modprueba.ModPrueba;
 
-public class ModBlock {
+// import net.minecraft.world.level.block.Block;
+// import net.minecraftforge.eventbus.api.IEventBus;
+// import net.minecraftforge.fmllegacy.RegistryObject;
+// import net.minecraftforge.registries.DeferredRegister;
+// import net.minecraftforge.registries.ForgeRegistries;
+
+// import java.util.function.Supplier;
+
+// public class ModBlock {
+//     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ModPrueba.Mod_ID);
     
-} */
+//     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+//         RegistryObject<T> toReturn = BLOCKS.register(name, block);
+//         return toReturn;
+//     }
+
+//     private static <T extends Block> void registerBlockitem(String name, RegistryObject<T> block) {
+//         ModItems.
+//     }
+
+//     public static void register(IEventBus eventBus) {
+//         BLOCKS.register(eventBus);
+//     }
+// } 
