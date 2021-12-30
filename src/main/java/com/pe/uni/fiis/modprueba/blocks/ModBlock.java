cbbe01fbc1502f44532bf9502b8a7593d5f19696
Oscar <oscar.torres.m@uni.pe>
2021-12-29 21:21:18 -0500

    Creado el item StickAcosta

---
+++ src/main/java/com/pe/uni/fiis/modprueba/blocks/ModBlock.java
@@ -0,0 +1,11 @@
+/*package com.pe.uni.fiis.modprueba.blocks;
+
+import org.antlr.v4.parse.ANTLRParser.blockEntry_return;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+
+public class ModBlock {
+    
+} */
