1437e60f23a7b475974617a17cdebd0f98c499ae
Oscar <oscar.torres.m@uni.pe>
2021-12-30 15:02:29 -0500

    Agregando comentarios a ModItem.java

--- src/main/java/com/pe/uni/fiis/modprueba/item/ModItem.java
+++ src/main/java/com/pe/uni/fiis/modprueba/item/ModItem.java
@@ -12,11 +12,22 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItem {
 
-
+    //creacion del registro de items del mod
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModPrueba.Mod_ID);
     
+
+    //agregando items basicos al registro
     public static final RegistryObject<Item> STICKACOSTA = ITEMS.register("stickacostaa",
             () -> new Item(new Item.Properties().stacksTo(16).tab(CreativeModeTab.TAB_MISC)));
+
+    //agregando herramientas
+    
+    //agregando armas al registro
+
+    //agregando items finales/especiales
+
+    //agregando huevos spawners
+
 
    public static void register(IEventBus eventBus) {
        ITEMS.register(eventBus);
