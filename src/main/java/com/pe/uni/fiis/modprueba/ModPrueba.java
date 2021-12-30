901e1ba4576c7b93ed986b8a18630a7de7b7649b
Oscar <oscar.torres.m@uni.pe>
2021-12-30 15:18:12 -0500

    Agregado el tab del modo creativo con los items del mod

--- src/main/java/com/pe/uni/fiis/modprueba/ModPrueba.java
+++ src/main/java/com/pe/uni/fiis/modprueba/ModPrueba.java
@@ -1,23 +1,16 @@
 package com.pe.uni.fiis.modprueba;
 
 
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 import com.pe.uni.fiis.modprueba.item.ModItem;
 
@@ -29,6 +22,7 @@ public class ModPrueba
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String Mod_ID = "modprueba";
+    public static final CreativeModeTab ACOSTA_TAB = new AcostaGroup("acosta_tab");
 
     public ModPrueba() {
         
@@ -45,5 +39,18 @@ public class ModPrueba
     private void setup(final FMLCommonSetupEvent event){
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    public static class AcostaGroup extends CreativeModeTab{
+
+        public AcostaGroup(String label) {
+            super(label);
+        }
+
+        @Override
+        public ItemStack makeIcon() {
+            return ModItem.STICKACOSTA.get().getDefaultInstance();
+        }
+
     }
 }
--- src/main/java/com/pe/uni/fiis/modprueba/item/ModItem.java
+++ src/main/java/com/pe/uni/fiis/modprueba/item/ModItem.java
@@ -2,10 +2,8 @@ package com.pe.uni.fiis.modprueba.item;
 
 import com.pe.uni.fiis.modprueba.ModPrueba;
 
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
-
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,11 +12,10 @@ public class ModItem {
 
     //creacion del registro de items del mod
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModPrueba.Mod_ID);
-    
 
     //agregando items basicos al registro
     public static final RegistryObject<Item> STICKACOSTA = ITEMS.register("stickacostaa",
-            () -> new Item(new Item.Properties().stacksTo(16).tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().stacksTo(16).tab(ModPrueba.ACOSTA_TAB)));
 
     //agregando herramientas
     
